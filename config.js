92d92326e0d7bf76d0a8a85f0e0f688e084a7a59
zainumd <oneminzain@gmail.com>
2025-07-12 17:29:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 /*
 
-# Base By Spider v7
-# Youtube= Tech God
+# Base By Zainu-MD
+# Youtube= Zainu-MD
 !- do not delete this credit
 
 */
 
 global.prefa = ['','!','.',',','🐸','🗿']
-global.owner = ['910000000000']
-global.ownMain = '910000000000'
-global.NamaOwner = 'TechGodSir' //your name
+global.owner = ['923360679553']
+global.ownMain = '923360679553'
+global.NamaOwner = 'ZAINU-MD' //ZAINU-MD
 global.sessionName = 'session'
 global.connect = true // for qr code do this false
-global.namabot = 'TECH-GOD-9' //don't change
-global.author = 'TechGodSir' //don't change
-global.packname = 'TechGodSir' //sticker name
+global.namabot = 'ZAINU-MD' //don't change
+global.author = 'ZAINU-MD' //don't change
+global.packname = 'ZAINU-MD' //sticker name
 global.domain = '' // Isi Domain Lu
 global.apikey = 'ptla_F9N0OMtFjU0fbiO04EJiPUcw3XaPgMUmJZA6cAk3zt8' // Isi Apikey Plta Lu
 global.capikey = 'ptlc_4SNLfuJidXDSWPx1LeIdY3TrFrcTaUcWqcJ92uORpTV' // Isi Apikey Pltc Lu
 global.eggsnya = '15' // id eggs yang dipakai
 global.location = '1' // id location
-global.url1 = 'https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h' //don't change this
-global.url2 = 'https://www.youtube.com/@techgod143' //Don't change this
-global.linkgc = 'https://whatsapp.com/channel/0029VahKfXr8aKvG3ctVjG1z'
+global.url1 = 'https://whatsapp.com/channel/0029Vb5ooWm6LwHq7ivWSF0C' //don't change this
+global.url2 = 'https://whatsapp.com/channel/0029Vb5ooWm6LwHq7ivWSF0C' //Don't change this
+global.linkgc = 'https://whatsapp.com/channel/0029Vb5ooWm6LwHq7ivWSF0C'
 global.delayjpm = 3500
 
 global.mess = { // Don't change this
 ingroup: 'This feature can only be used in groups.',
 admin: 'This feature is specifically for group admins.',
 notadmin: "The bot must be an admin first",
-owner: 'You are not owner or Tech God.',
+owner: 'You are not owner or ZAINU-MD.',
 premium: 'You are not a premium user or Tech God.',
 seller: 'This feature can only be used by resellers and tech god.',
 usingsetpp: `Setpp can only be used by the owner, do you think I'm stupid?`,
